828059932b769732dfabc4f5736ec29e3ae6f3bc
PabloTToledano <pablo.toledano@uma.es>
2023-04-07 21:34:39 +0200

    front show chargers and connector status

--- FrontHttp/app.py
+++ FrontHttp/app.py
@@ -1,31 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    items = [
-        {
-            "color": "bg-danger",
-            "name": "Cargador 1",
-            "reverse_status": "Reserved",
-            "url": "google.es",
-        },
-        {
-            "color": "bg-success",
-            "name": "Cargador 2",
-            "reverse_status": "Free",
-            "url": "google.es",
-        },
-        {
-            "color": "bg-success",
-            "name": "Cargador 3",
-            "reverse_status": "Free",
-            "url": "google.es",
-        },
-    ]
+    url = "http://localhost:8080/chargers"
+    response = requests.get(url)
+    json_data = response.json()
+    items = []
+    for charger in json_data:
+        status = "Free"
+        for connector in json_data[charger].get("connectors"):
+            if connector == "Reserved":
+                status = "Reserved"
+        if status == "Reserved":
+            color = "bg-danger"
+        else:
+            color = "bg-success"
+
+        items.append(
+            {
+                "color": color,
+                "name": json_data[charger].get("ChargerStation").get("model"),
+                "reverse_status": status,
+                "url": f"charger?id={charger}",
+                "button_text": "View",
+            }
+        )
     return render_template("home.html", items=items)
+
+
+@app.route("/charger")
+def charger():
+    url = "http://localhost:8080/chargers"
+    response = requests.get(url)
+    json_data = response.json()
+    items = []
+    status = "Free"
+    charger_id = request.args.get("id", default="cp", type=str)
+    for connector in json_data[charger_id].get("connectors"):
+        status = json_data[charger_id]["connectors"][connector]
+        if status == "Reserved":
+            color = "bg-danger"
+        else:
+            color = "bg-success"
+        items.append(
+            {
+                "color": color,
+                "name": f"Connector {connector}",
+                "reverse_status": status,
+                "url": "reserve",
+                "button_text": "Reserve",
+            }
+        )
+
+    return render_template("home.html", items=items)
+
+
+@app.route("/logo.png")
+def get_image():
+    return send_file("images/logo.png", mimetype="image/png")
+
+
+@app.route("/favicon.ico")
+def get_ico():
+    return send_file("images/favicon.ico", mimetype="image/x-icon")
 
 
 if __name__ == "__main__":
